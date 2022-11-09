4d93fe47ed3dea6f8f27f5f69853ac364668e585
mihai <doloiu22@icloud.com>
2022-11-09 21:47:19 +0200

    Fourth testing commit of the VS Project

--- ProjectModernCpp/ProjectModernCpp/ProjectModernCpp.cpp
+++ ProjectModernCpp/ProjectModernCpp/ProjectModernCpp.cpp
@@ -1,5 +1,26 @@
 ﻿#include "ProjectModernCpp.h"
 
+class Test {
+
+private:
+	int test1;
+	int test2;
+	double test3;
+
+public:
+	Test() {
+		test1 = 1;
+		test2 = 2;
+		test3 = 3.3;
+	}
+	void test() {
+		std::cout << test1;
+		std::cout << test2;
+		std::cout << test3;
+	}
+
+};
+
 int main()
 {
 	std::cout << "Testing\n";
