88bcd22527d95d58ea41bfcb76c6c635e42fd4f6
rdmc10 <radu.m10@protonmail.com>
2022-11-12 16:50:54 +0200

    Created header for User class

--- ProjectModernCpp/ProjectModernCpp/User.h
+++ ProjectModernCpp/ProjectModernCpp/User.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+#include <cstdint>
+class User
+{
+private:
+	uint32_t m_userId;
+	std::string m_username;
+	std::string m_firstName;
+	std::string m_lastName;
+	std::string m_email;
+	std::string m_birthDate;
+	//TODO: categories/favorite actors etc
+
+
+};
+
