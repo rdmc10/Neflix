67353acc9f737ba1d6dbd453527d37b4946ed30f
mihai <doloiu22@icloud.com>
2022-11-12 22:32:19 +0200

    Added implementation for setters in Movie class

--- ProjectModernCpp/ProjectModernCpp/Movie.cpp
+++ ProjectModernCpp/ProjectModernCpp/Movie.cpp
@@ -1,7 +1,6 @@
 #pragma once
 #include "Movie.h"
 
-
 uint32_t Movie::GetMovieID() const
 {
 	return m_movieId;
@@ -30,4 +29,34 @@ uint16_t Movie::GetDuration() const
 std::string Movie::GetDescription() const
 {
 	return m_description;
+}
+
+void Movie::SetID(const uint32_t& id)
+{
+	m_movieId = id;
+}
+
+void Movie::SetType(const Type& type)
+{
+	m_type = type;
+}
+
+void Movie::SetName(const std::string& name)
+{
+	m_name = name;
+}
+
+void Movie::SetReleaseDate(const uint16_t& releaseDate)
+{
+	m_releaseDate = releaseDate;
+}
+
+void Movie::SetDuration(const uint16_t& duration)
+{
+	m_duration = duration;
+}
+
+void Movie::SetDescription(const std::string& description)
+{
+	m_description = description;
 }
