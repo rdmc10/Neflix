18d1c73937ac495b517014aff133596a33580f0d
rdmc10 <radu.m10@protonmail.com>
2022-11-19 16:20:46 +0200

    Implemented getters for Person class

--- ProjectModernCpp/ProjectModernCpp/Person.cpp
+++ ProjectModernCpp/ProjectModernCpp/Person.cpp
@@ -15,7 +15,27 @@ void Person::SetRole(const Role& role)
 	m_role = role;
 }
 
-void Person::SetMovie(const std::set<Movie> movieList)
+void Person::SetMovie(const std::set<Movie>& movieList)
 {
 	m_movieList = movieList;
+}
+
+std::string Person::GetFirstName() const
+{
+	return m_firstName;
+}
+
+std::string Person::GetLastName() const
+{
+	return m_lastName;
+}
+
+Person::Role Person::GetRole() const
+{
+	return m_role;
+}
+
+std::set<Movie> Person::GetMovieList() const
+{
+	return m_movieList;
 }
--- ProjectModernCpp/ProjectModernCpp/Person.h
+++ ProjectModernCpp/ProjectModernCpp/Person.h
@@ -15,7 +15,11 @@ public:
 	void SetFirstName(const std::string& firstName);
 	void SetLastName(const std::string& lastName);
 	void SetRole(const Role& role);
-	void SetMovie(const std::set<Movie> movieList);
+	void SetMovie(const std::set<Movie>& movieList);
+	std::string GetFirstName() const;
+	std::string GetLastName() const;
+	Role GetRole() const;
+	std::set<Movie> GetMovieList() const;
 
 private:
 	std::string m_firstName;
