7808d3aa361be7f97fe2ca42688e0c49521515bf
mihai <doloiu22@icloud.com>
2022-11-19 18:02:05 +0200

    Added implementation for getters, setters and constructors for MovieCategory class

--- ProjectModernCpp/ProjectModernCpp/MovieCategory.cpp
+++ ProjectModernCpp/ProjectModernCpp/MovieCategory.cpp
@@ -1,1 +1,33 @@
 #include "MovieCategory.h"
+
+MovieCategory::MovieCategory(const MovieCategory& movieCategory)
+	: m_id(movieCategory.m_id)
+	, m_name(movieCategory.m_name)
+{
+}
+
+MovieCategory::MovieCategory(const uint32_t& id, const std::string& name)
+	: m_id(id)
+	, m_name(name)
+{
+}
+
+void MovieCategory::SetId(const uint32_t& id)
+{
+	m_id = id;
+}
+
+void MovieCategory::SetName(const std::string& name)
+{
+	m_name = name;
+}
+
+uint32_t MovieCategory::GetId() const
+{
+	return m_id;
+}
+
+std::string MovieCategory::GetName() const
+{
+	return m_name;
+}
--- ProjectModernCpp/ProjectModernCpp/MovieCategory.h
+++ ProjectModernCpp/ProjectModernCpp/MovieCategory.h
@@ -7,7 +7,7 @@ public:
 	MovieCategory() = default;
 	MovieCategory(const MovieCategory& movieCategory);
 	MovieCategory(const uint32_t& id, const std::string& name);
-	void SetId(const uint32_t& m_id);
+	void SetId(const uint32_t& id);
 	void SetName(const std::string& name);
 	uint32_t GetId() const;
 	std::string GetName() const;
