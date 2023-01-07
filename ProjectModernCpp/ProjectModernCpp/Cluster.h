ae776b09493442a12be1058d85b8c6d5e71b2d46
mboboo <hitnt@proton.me>
2023-01-07 16:56:42 +0200

    Added class Cluster

--- ProjectModernCpp/ProjectModernCpp/Cluster.h
+++ ProjectModernCpp/ProjectModernCpp/Cluster.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <crow.h>
+#include <cstdint>
+
+using Position = std::pair<float, float>;
+
 class Cluster
 {
+
+public:
+	Cluster(uint8_t id, Position clusterPosition);
+
+	void AddPoint(Position positionPoint);
+	void UpdateClusterPosition();
+	void RemovePoints();
+
+	Position GetPosition() const;
+
+private:
+	uint8_t m_id;
+	std::pair<float, float> m_clusterPosition;
+	std::vector<Position> m_points;
 };
 
