da1689cd65483c2acae82219022428a05c89edc1
rdmc10 <radu.m10@protonmail.com>
2023-01-07 23:16:05 +0200

    Added a split function that uses boost in the ParseUtil header

--- ProjectModernCpp/ProjectModernCpp/ParseUtil.h
+++ ProjectModernCpp/ProjectModernCpp/ParseUtil.h
@@ -2,6 +2,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <regex>
+#include <sstream>
+#include <boost/algorithm/string.hpp>
+
 
 std::vector<std::string> ParseString (const std::string& str) {
     std::vector<std::string> result;
@@ -17,4 +21,11 @@ std::string GetFirstWord(const std::string& str) {
     size_t spacePos = str.find(' ');
     std::string firstWord = str.substr(0, spacePos);
     return firstWord;
+}
+
+std::vector<std::string> SplitString (const std::string& string,const std::string& separator) {
+    std::vector<std::string> strs;
+    boost::split(strs, string, boost::is_any_of(separator));
+
+    return strs;
 }
